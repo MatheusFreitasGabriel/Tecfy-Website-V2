98cbe506bff0f3dfe0df99b7ba7f810d5cc49fa5
MatheusFreitasGabriel <matheusfreitasdev@hotmail.com>
2026-04-20 18:01:18 -0300

    feat: endpoint api debug

--- src/services/api/apiConfig.ts
+++ src/services/api/apiConfig.ts
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = "https://31rwlm1ske.execute-api.us-east-1.amazonaws.com/api/v1";
+const DEFAULT_API_BASE_URL = "https://zc5ogdousj.execute-api.us-east-1.amazonaws.com/api/v1";
 
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL?.trim() || DEFAULT_API_BASE_URL;
